9505f7571bc1645989c5bbdfe9ce6cbbbe43da81
alexdev20 <alex.sousa@innyx.com>
2022-11-15 16:27:37 -0300

    Fomulário de cadastro

--- src/main/java/vr/miniautorizador/entity/form/CardForm.java
+++ src/main/java/vr/miniautorizador/entity/form/CardForm.java
@@ -1,4 +1,20 @@
 package vr.miniautorizador.entity.form;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CardForm {
+
+    private String numberCard;
+
+    private String passwordCard;
+
+    private BigDecimal value;
+
 }
