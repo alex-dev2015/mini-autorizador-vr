d8b6474d34d68c4d02453d8f2fbe737c0fdfc3e9
alexdev20 <alex.sousa@innyx.com>
2022-11-15 16:29:51 -0300

    Interface do cartão

--- src/main/java/vr/miniautorizador/service/ICardService.java
+++ src/main/java/vr/miniautorizador/service/ICardService.java
@@ -1,4 +1,20 @@
 package vr.miniautorizador.service;
 
+import vr.miniautorizador.entity.Card;
+import vr.miniautorizador.entity.form.CardForm;
+
 public interface ICardService {
+    /**
+     * Cria um cartão e salva no banco de dados.
+     * @param form - Formulário referente aos dados para a criação de um Cartão no banco de dados.
+     * @return - Cartão recém-criado.
+     */
+    Card create(CardForm form);
+
+    /**
+     * Retorna um cartão que está no banco de dados de acordo com o seu número
+     * @param numberCard - Número do cartão.
+     * @return - Dados do cartão.
+     */
+    Card get(String numberCard);
 }
