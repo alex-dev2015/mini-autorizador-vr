7694e3e384f9178b1f128fa16ff8ca8dd5bde409
alexdev20 <alex.sousa@innyx.com>
2022-11-15 12:24:45 -0300

    Criando a entidade

--- src/main/java/vr/miniautorizador/entity/Card.java
+++ src/main/java/vr/miniautorizador/entity/Card.java
@@ -1,4 +1,27 @@
 package vr.miniautorizador.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_cards")
 public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String numberCard;
+
+    private String passwordCard;
+
+    private BigDecimal value;
 }
