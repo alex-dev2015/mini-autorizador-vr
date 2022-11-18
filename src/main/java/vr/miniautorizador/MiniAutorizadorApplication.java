ea0c6e9fa67754d43c773caa2cef7854d2342b3d
alexdev20 <alex.sousa@innyx.com>
2022-11-18 17:09:19 -0300

    Documentação do projeto

--- src/main/java/vr/miniautorizador/MiniAutorizadorApplication.java
+++ src/main/java/vr/miniautorizador/MiniAutorizadorApplication.java
@@ -1,6 +1,11 @@
 package vr.miniautorizador;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -10,4 +15,14 @@ public class MiniAutorizadorApplication {
         SpringApplication.run(MiniAutorizadorApplication.class, args);
     }
 
+    @Bean
+    public OpenAPI customOpenAPI(@Value("Api Mini Autorizador") String description){
+        return new OpenAPI()
+                .info(new Info()
+                        .title(description)
+                        .version("1.0")
+                        .termsOfService("http://suagger.io/terms")
+                        .license(new License().name("Apache 2.0")
+                                .url("http://springdoc.org")));
+    }
 }
